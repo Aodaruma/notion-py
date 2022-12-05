eb26aef4c39fb9c0461517444613d64d0d5a5498
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2022-12-05 20:33:24 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="notion-py",
+    version="1.0.0",
+    description="A Python wrapper for the Notion API",
+    url="https://github.com/Aodaruma/notion-py",
+    author="Aodaruma",
+    author_email="aodaruma.contact@gmail.com",
+    license="GPLv3",
+    packages=[],
+    install_requires=[
+        "requests>=2.22.0",
+    ],
+    zip_safe=False,
+)
